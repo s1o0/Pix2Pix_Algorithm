39691fd59984219e2e5cb4c1d12edc20f5704e12
s1o0 <simonbeurel@protonmail.com>
2022-04-13 14:34:53 +0100

    Premier test pour la transition d'image

--- Transition_d_image/transition_image.py
+++ Transition_d_image/transition_image.py
@@ -4,14 +4,14 @@ import os
 
 
 #Transition de l'image pleiade vers le lambert
-filename = r"C:\path\to\input\raster
+filename = "IMG_PHR1A_MS_201802121120006_SEN_2710546101-002_R1C1.JP2"
 input_raster = gdal.Open(filename)
-output_raster = r"C:\path\to\output\raster
+output_raster = "IMG_PHR1A_MS_201802121120006_SEN_2710546101-002_R1C1.JP2"
 warp = gdal.Warp(output_raster,input_raster,dstSRS='EPSG:2154')
 warp = None 
 
 #Obtenir les infos de l'images pleiade transformée
-src = gdal.Open(path goes here)
+src = gdal.Open("IMG_PHR1A_MS_201802121120006_SEN_2710546101-002_R1C1.JP2")
 ulx, xres, xskew, uly, yskew, yres  = src.GetGeoTransform()
 lrx = ulx + (src.RasterXSize * xres)
 lry = uly + (src.RasterYSize * yres)
@@ -20,5 +20,6 @@ lry = uly + (src.RasterYSize * yres)
 #Crop l'image ign ou avion
 window = (ulx,uly,lrx,lry)
 
-gdal.Translate('output_crop_raster.tif', 'input_raster.tif', projWin = window)
-
+ds = gdal.Open('IMG_PHR1A_MS_201802121120006_SEN_2710546101-002_R1C1.JP2')
+ds = gdal.Translate('output.tif', ds, projWin = window)
+ds = None
