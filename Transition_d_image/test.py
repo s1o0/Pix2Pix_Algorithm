b46f5bd149462b31d21440a7cf40f2a84a01b841
s1o0 <simonbeurel@protonmail.com>
2022-04-14 16:51:00 +0100

    test.py

---
+++ Transition_d_image/test.py
@@ -0,0 +1,30 @@
+from osgeo import gdal,ogr,osr
+
+def GetExtent(ds):
+    """ Return list of corner coordinates from a gdal Dataset """
+    xmin, xpixel, _, ymax, _, ypixel = ds.GetGeoTransform()
+    width, height = ds.RasterXSize, ds.RasterYSize
+    xmax = xmin + width * xpixel
+    ymin = ymax + height * ypixel
+
+    return (xmin, ymax), (xmax, ymax), (xmax, ymin), (xmin, ymin)
+
+def ReprojectCoords(coords,src_srs,tgt_srs):
+    """ Reproject a list of x,y coordinates. """
+    trans_coords=[]
+    transform = osr.CoordinateTransformation( src_srs, tgt_srs)
+    for x,y in coords:
+        x,y,z = transform.TransformPoint(x,y)
+        trans_coords.append([x,y])
+    return trans_coords
+
+raster=r'IMG_PHR1B_MS_002/IMG_PHR1B_MS_202104211119066_SEN_6005996101-2_R1C1.JP2'
+ds=gdal.Open(raster)
+ext=GetExtent(ds)
+src_srs=osr.SpatialReference()
+src_srs.ImportFromWkt(ds.GetProjection())
+#tgt_srs=osr.SpatialReference()
+#tgt_srs.ImportFromEPSG(4326)
+tgt_srs = src_srs.CloneGeogCS()
+print("1")
+geo_ext=ReprojectCoords(ext, src_srs, tgt_srs)
