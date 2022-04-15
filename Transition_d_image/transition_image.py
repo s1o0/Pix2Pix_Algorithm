4d1f8a351e1d461dfc31d98583edf6b04b2f2353
s1o0 <simonbeurel@protonmail.com>
2022-04-15 16:36:52 +0100

    v2 transition_image

--- Transition_d_image/transition_image.py
+++ Transition_d_image/transition_image.py
@@ -21,15 +21,15 @@ with open(path_to_ign_data, 'r') as fp:
 
             x_haut_gauche = float(x_haut_gauche_string)
             y_haut_gauche = float(y_haut_gauche_string)
-        if i ==10:
+        elif i==10:
             lhs, rhs = line.split(" ", 1)
             twoparse = lhs.split(",")
             x_bas_droite_string = twoparse[0].replace('(', '')
-            y__bas_droite_string = twoparse[1].replace(')', '')
+            y_bas_droite_string = twoparse[1].replace(')', '')
 
-            x_bas_droite = float(x_haut_gauche_string)
-            y_bas_droite = float(y_haut_gauche_string)
-        if i > 10:
+            x_bas_droite = float(x_bas_droite_string)
+            y_bas_droite = float(y_bas_droite_string)
+        elif i > 10:
             # don't read after line 10 to save time
             break
 
@@ -37,12 +37,27 @@ proj = pyproj.Transformer.from_crs(2154, 4326, always_xy=True)
 x_haut_gauche,y_haut_gauche = proj.transform(x_haut_gauche,y_haut_gauche)
 x_bas_droite,y_bas_droite = proj.transform(x_bas_droite,y_bas_droite)
 
+print("*---------------*")
+print(x_haut_gauche)
+print(y_haut_gauche)
+print()
+print(x_bas_droite)
+print(y_bas_droite)
+
+
 x_min = min(x_bas_droite,x_haut_gauche)
 x_max = max(x_bas_droite,x_haut_gauche)
 
 y_min = min(y_bas_droite,y_haut_gauche)
 y_max = max(y_bas_droite,y_haut_gauche)
 
+print("*--------------*")
+print()
+print(x_min)
+print(y_min)
+print()
+print(x_max)
+print(y_max)
 
 path_to_new_pleiade_tif = input("Path to new pleiade tif")
 os.system(f'gdalwarp -s_srs EPSG:4326 -t_srs EPSG:4326 -te {x_min} {y_min} {x_max} {y_max} -te_srs EPSG:4326 {path_to_file_pleiade} {path_to_new_pleiade_tif}')
